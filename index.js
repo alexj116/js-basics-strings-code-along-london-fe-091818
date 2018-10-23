20b95fcf3c037a471519812919d5ec4c6f5b524b
Alexandra Johnson <alexandra@queryclick.com>
2018-10-23 14:29:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const currentUser="Grace Hopper"
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = "Welcome,$[currentUser[0]!";
+const shortGreeting = "Welcome, ${firstInitial}!";
 
 const firstInitial = currentUser[0];
 const restOfName = currentUser.slice(1);
