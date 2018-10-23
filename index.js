eadb32153d2d1f1de8d6acf762350fd5903b8b60
Alexandra Johnson <alexandra@queryclick.com>
2018-10-23 14:00:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 const currentUser="Grace Hopper"
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-const upperCaseCurrentUser = currentUser.toUpperCase
+const upperCaseCurrentUser = currentUser.toUpperCase()
 const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!"
